f9d834abfd7bc793904c0a59b4040fd724c3201e
tjrudrnr2 <tjrudrnr2@hanmail.net>
2025-03-11 14:24:57 +0900

    Init project page

--- models/model_utils.py
+++
@@ -1,9 +0,0 @@
-from collections import OrderedDict
-
-def MADA(args, aggregated_weights, global_model, beta=0.5):
-    ema_weights = OrderedDict()
-    
-    for key in aggregated_weights.keys():
-        ema_weights[key] = beta * aggregated_weights[key] + (1.0 - beta) * global_model[key]
-
-    return ema_weights
